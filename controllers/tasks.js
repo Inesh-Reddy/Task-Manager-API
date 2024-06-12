f71e4d757cda465a3f4cdd0dc7675adb207fdc61
Inesh-Reddy <ineshreddy007@gmail.com>
2024-06-12 17:24:10 +0530

    adding rest of the routes controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res)=>{
     res.send("All tasks are here!")
 }
 
+const createTask = (req, res)=>{
+    res.json(req.body);
+}
+
+const getSingleTask = (req, res)=>{
+    res.json(`id: ${req.params.id}`);
+}
+
+const updateTask = (req, res)=>{
+    res.send(`Updated task`);
+}
+
+const deleteTask = (req, res)=>{
+    res.send(`Deleted taks`);
+}
+
 module.exports ={
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getSingleTask,
+    updateTask,
+    deleteTask
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getAllTasks } = require('../controllers/tasks');
+const { getAllTasks, createTask, getSingleTask, updateTask, deleteTask } = require('../controllers/tasks');
 const router = express.Router();
 
-router.route('/').get(getAllTasks)
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getSingleTask).patch(updateTask).delete(deleteTask);
 
 module.exports= router
